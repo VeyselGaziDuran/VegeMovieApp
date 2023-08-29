9c42d1a9cbdea6d4f8659a0917e70ddea3dce1a3
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-08-29 23:11:42 +0300

    Popular TV Shows

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
   currentPage: window.location.pathname
 };
 
+//! Display Popular Movies
 async function displayPopularMovies() {
   const { results } = await fetchData('movie/popular');
   results.forEach((movie) => {
@@ -25,6 +26,35 @@ async function displayPopularMovies() {
             </div>
       `;
     document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+//! Display Popular TV Shows
+async function displayPopularShows() {
+  const { results } = await fetchData('tv/popular');
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+              <a href="tv-details.html?id=${show.id}">
+                ${
+                  show.poster_path
+                    ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" 
+                    class="card-img-top" alt="${show.name}">`
+                    : `<img src="../images/no-image.jpg"
+                    class="card-img-top" alt="${show.name}">`
+                }
+              </a>
+              <div class="card-body">
+                <h5 class="card-title">${show.name}</h5>
+                <p class="card-text">
+                  <small class="text-muted">Air Date: ${
+                    show.first_air_date
+                  }</small>
+                </p>
+              </div>
+        `;
+    document.querySelector('#popular-shows').appendChild(div);
   });
 }
 
@@ -73,7 +103,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows Page');
+      displayPopularShows();
       break;
     case '/tv-details.html':
       console.log('TV Details Page');
